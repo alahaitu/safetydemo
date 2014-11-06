f59c277cdd214a8194a0c76102330b31f18192dd
lepya <lauri.nykanen@gmail.com>
2014-11-06 21:10:29 +0200

    eating game
    ...

--- game/states/eatingScene.js
+++ game/states/eatingScene.js
@@ -11,8 +11,8 @@ var badEatObject = require('../prefabs/badEatObject');
 
         this.eating_background = this.game.add.sprite(0, 0, 'eating_bg');
         this.backButton = this.add.button(899, 23, 'exit_btn' , this.startPlayroom, this);
-        this.backButton = this.add.button(899, 23, 'pointer' , this.startPlayroom, this);
-        this.backButton = this.add.button(899, 23, 'score_meter' , this.startPlayroom, this);
+        this.scoreMeter = this.game.add.sprite(200, 35, 'score_meter');
+        this.pointer = this.game.add.sprite(215, 37, 'pointer');
 
         this.objectGenerator = this.game.time.events.loop(Phaser.Timer.SECOND * this.game.rnd.integerInRange(3, 4.5), this.generateObjects, this);
 
