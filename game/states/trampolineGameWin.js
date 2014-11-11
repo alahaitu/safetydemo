43165dd48c757f324caa184cf393a69b05e601d3
alahaitu <tuomas.alahaivala@oulu.fi>
2014-11-11 22:39:38 +0200

    Space game winning scene

--- game/states/trampolineGameWin.js
+++ game/states/trampolineGameWin.js
@@ -3,6 +3,8 @@
   TrampolineGameWin.prototype = {
     create: function() {
       this.add.sprite(0, 0, 'trampoline_game_win');
+      //this.add.sprite(180,0, 'trampoline_game_win_glow');      
+      this.add.sprite(180,0, 'trampoline_game_win_alien');
       this.applauseSound = this.add.audio('applause_sound');
       this.game.time.events.add(Phaser.Timer.SECOND * 3, this.startPlayground, this);
       this.applauseSound.play();
