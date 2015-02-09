4ef1a8b4a8f14a1a97fb8964eac48e5e79f24083
lepya <lauri.nykanen@gmail.com>
2015-02-09 18:50:50 +0200

    removed redundant states, prefabs

--- game/main.js
+++ game/main.js
@@ -5,24 +5,17 @@ window.onload = function () {
   var game = new Phaser.Game(1024, 688, Phaser.AUTO, 'safety-demo');
 
   // Game States
-  game.state.add('beachScene', require('./states/beachScene'));
   game.state.add('boot', require('./states/boot'));
   game.state.add('credits', require('./states/credits'));
   game.state.add('eating', require('./states/eating'));
   game.state.add('eatingGameWin', require('./states/eatingGameWin'));
-  game.state.add('eatingScene', require('./states/eatingScene'));
-  game.state.add('gameover', require('./states/gameover'));
   game.state.add('lifejacket', require('./states/lifejacket'));
   game.state.add('lifejacketWin', require('./states/lifejacketWin'));
-  game.state.add('menu', require('./states/menu'));
-  game.state.add('play', require('./states/play'));
   game.state.add('playroom', require('./states/playroom'));
   game.state.add('preload', require('./states/preload'));
   game.state.add('spaceRunWin', require('./states/spaceRunWin'));
   game.state.add('spaceScene', require('./states/spaceScene'));
   game.state.add('spaceStation', require('./states/spaceStation'));
-  game.state.add('trampoline', require('./states/trampoline'));
-  game.state.add('trampolineCutscene', require('./states/trampolineCutscene'));
   
 
   game.state.start('boot');
