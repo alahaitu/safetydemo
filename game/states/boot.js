ebf7fef55086ac2ec6522d4eb8871b1d21308995
lepya <lauri.nykanen@gmail.com>
2015-02-09 13:39:52 +0200

    removed mobile minHeight,width & unused code

--- game/states/boot.js
+++ game/states/boot.js
@@ -29,32 +29,15 @@ Boot.prototype = {
     else
     {
         this.scale.scaleMode = Phaser.ScaleManager.EXACT_FIT;
-        this.scale.minWidth = 480;
-        this.scale.minHeight = 260;
         this.scale.maxWidth = 1024;
         this.scale.maxHeight = 688;
         this.scale.pageAlignHorizontally = true;
         this.scale.pageAlignVertically = true;
         this.scale.forceOrientation(true, false);
-        //this.scale.hasResized.add(this.gameResized, this);
-        //this.scale.enterIncorrectOrientation.add(this.enterIncorrectOrientation, this);
-        //this.scale.leaveIncorrectOrientation.add(this.leaveIncorrectOrientation, this);
         this.scale.setScreenSize(true);
     }
 
     this.game.state.start('preload');
-  },
-  gameResized: function (width, height) {
-      //  This could be handy if you need to do any extra processing if the game resizes.
-      //  A resize could happen if for example swapping orientation on a device.
-  },
-  enterIncorrectOrientation: function () {
-      BasicGame.orientated = false;
-      document.getElementById('orientation').style.display = 'block';
-  },
-  leaveIncorrectOrientation: function () {
-      BasicGame.orientated = true;
-      document.getElementById('orientation').style.display = 'none';
   }
 };
 
